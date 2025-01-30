babff71fefa5c03ff5e5f31181b49a82b6a2e1c5
vitalii-pc <alievruslan1818@gmail.com>
2025-01-30 22:45:26 +0100

    add_camperdetails

--- src/components/CamperDetails/CamperDetails.jsx
+++ src/components/CamperDetails/CamperDetails.jsx
@@ -1,35 +1,38 @@
+import { useState } from "react";
+import Modal from "../Modal/Modal.jsx";
+import RatingAndLocation from "../RatingAndLocation/RatingAndLocation.jsx";
+import css from "./CamperDetails.module.css";
+
 const CamperDetails = () => {
+	const [modalIsOpen, setIsOpen] = useState(false);
+
 	return (
 		<>
-			{modalIsOpen && (
-				<Modal
-					modalData={modalData}
-					modalIsOpen={modalIsOpen}
-					closeModal={onModalClose}
-				/>
-			)}
+			{modalIsOpen && <Modal />}
 			<div className={css.camperDetails}>
-				<h2 className={css.name}>{name}</h2>
-				<RatingAndLocation
-					rating={rating}
-					reviews={reviews}
-					location={location}
-				/>
-				<h2 className={css.price}>€{price}.00</h2>
+				<h2 className={css.name}>Mavericks</h2>
+				<RatingAndLocation />
+				<h2 className={css.price}>€8000.00</h2>
 				<ul className={css.images}>
-					{gallery.map((photo) => (
+					{[].map((photo) => (
 						<li
 							key={photo.thumb}
 							onClick={() => {
 								setIsOpen(true);
-								setModalData(photo.original);
 							}}
 						>
 							<img src={photo.thumb} alt="camper photo" className={css.image} />
 						</li>
 					))}
 				</ul>
-				<p className={css.description}>{description}</p>
+				<p className={css.description}>
+					Embrace simplicity and freedom with the Mavericks panel truck, an
+					ideal choice for solo travelers or couples seeking a compact and
+					efficient way to explore the open roads. This no-frills yet reliable
+					panel truck offers the essentials for a comfortable journey, making it
+					the perfect companion for those who value simplicity and
+					functionality.
+				</p>
 			</div>
 		</>
 	);
