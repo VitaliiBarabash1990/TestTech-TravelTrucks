6dcd62adb9f054a484f46d1ab3146f1ee99fcba8
vitalii-pc <alievruslan1818@gmail.com>
2025-01-30 22:05:30 +0100

    add reting

--- src/components/RatingAndLocation/RatingAndLocation.jsx
+++ src/components/RatingAndLocation/RatingAndLocation.jsx
@@ -1,5 +1,22 @@
+import css from "./RatingAndLocation.module.css";
+
 const RatingAndLocation = () => {
-	return <div>RatingAndLocation</div>;
+	return (
+		<div className={css.groupRatingAndLocation}>
+			<div className={css.groupRating}>
+				<svg width={16} height={16} fill="#ffc531">
+					<use href="/symbol-defs.svg#icon-rating"></use>
+				</svg>
+				<p className={css.rating}>4.4(2 Reviews)</p>
+			</div>
+			<div className={css.groupLocation}>
+				<svg width={16} height={16} fill="#101828">
+					<use href="/symbol-defs.svg#icon-map"></use>
+				</svg>
+				<p className={css.rating}>Kyiv, Ukraine</p>
+			</div>
+		</div>
+	);
 };
 
 export default RatingAndLocation;
