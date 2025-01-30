480b56b0e6a985d26f18bad9079a691fb013b053
vitalii-pc <alievruslan1818@gmail.com>
2025-01-30 21:59:55 +0100

    add equipmentitem

--- src/components/EquipmentItem/EquipmentItem.jsx
+++ src/components/EquipmentItem/EquipmentItem.jsx
@@ -1,5 +1,13 @@
+import css from "./EquipmentItem.module.css";
+
 const EquipmentItem = () => {
-	return <div>EquipmentItem</div>;
+	return (
+		<li className={css.equipmentItem}>
+			<svg width={20} height={20} fill="#101828">
+				<use href={`/symbol-defs.svg#icon-${}`}></use>
+			</svg>
+		</li>
+	);
 };
 
 export default EquipmentItem;
